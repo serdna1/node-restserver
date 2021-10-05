e6c55fd7e59d94313fee1819dde6761abc2c5e24
Andrés Otero <andrescaot@gmail.com>
2021-10-05 14:44:46 +0200

    Google sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
-const {response} = require('express')
+const {response, json} = require('express')
 const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario')
+
 const { generarJWT } = require('../helpers/generar-jwt')
+const { googleVerify } = require('../helpers/google-verify')
 
 
 const login = async(req, res = response) => {
@@ -53,8 +55,55 @@ const login = async(req, res = response) => {
 }
 
 
+const googleSignIn = async(req, res = response) => {
+
+    const {id_token} = req.body
+
+    try {
+        const {correo, nombre, img} = await googleVerify(id_token)
+
+        let usuario = await Usuario.findOne({correo})
+
+        if (!usuario) {
+            // Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data)
+
+            await usuario.save()
+        }
+
+        // Si el usuario en DB
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id)
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar'
+        })
+    }
+
+}
+
 
 
 module.exports = {
-    login
+    login,
+    googleSignIn,
 }
--- models/usuario.js
+++ models/usuario.js
@@ -21,7 +21,8 @@ const UsuarioSchema = Schema({
     rol: {
         type: String,
         required: true,
-        enum: ['ADMIN_ROLE', 'USER_ROLE']
+        enum: ['ADMIN_ROLE', 'USER_ROLE'],
+        default: 'USER_ROLE'
     },
     estado: {
         type: Boolean,
